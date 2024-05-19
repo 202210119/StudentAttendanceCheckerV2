675dd284c44a5e568228d228da5d97ccb0e0151f
202210119 <202210119@feualabang.edu.ph>
2024-05-19 23:53:01 +0800

    update

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def display_class(class_name):
         gb = GridOptionsBuilder.from_dataframe(df)
         gb.configure_columns(df.columns.tolist()[:2], editable=True)  # Limit to the first 2 columns
         grid_options = gb.build()
-        grid_response = AgGrid(df, gridOptions=grid_options, columns_auto_size_mode=ColumnsAutoSizeMode.FIT_CONTENTS)
+        grid_response = AgGrid(df, gridOptions=grid_options)  # Remove columns_auto_size_mode argument
         updated_df = grid_response['data']
 
         
